eae1110ea438dedc1e14b7abc3a4e57eb520367c
Muhammad Ibrahim <jabraheemkhan557@gmail.com>
2024-12-16 16:19:36 -0800

    fixed some issuues

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation"; // Import useRouter
 import Image from "next/image";
 
